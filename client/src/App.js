f30379da532a9646613b398b98976c5f308fb5c5
aksharkh <aksharkh04@gmail.com>
2024-10-25 22:34:31 +0530

    creted frontend folder

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div>
+      
+        <Header />
+        <Sidebar />
+        
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AppBar, Toolbar} from '@mui/material'
+function Header() {
+  return (
+    <div>
+        <AppBar position='static'>
+            <Toolbar>
+
+            </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
+
+export default Header
